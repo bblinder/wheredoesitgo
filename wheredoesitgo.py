fd6d2dd9989751d20edc65f420410e12ef43b312
bblinder <filmmaven@gmail.com>
2022-08-24 10:07:57 -0400

    modified:   wheredoesitgo.py

--- wheredoesitgo.py
+++ wheredoesitgo.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 ############
 # This method is if you want to make a request via a browser form.
 
-
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -26,20 +25,22 @@ def index():
 @app.route('/redirect', methods=['POST'])
 def redirect_url():
     url = request.form['url']
-    if not url.startswith('http'):
-        formatted_url = 'http://' + url
-    r = requests.get(formatted_url, allow_redirects=True)
-    with open('history.log', 'a') as f:
-        f.write(f"Initial URL: {url} ; Final URL: {r.url} '\n'")
+    
+    # if not URL provided, return error and link back to index
+    if not url:
+        return '<a href="/">Please enter a URL</a>'
+    else:
+        if not url.startswith('http') or not url.startswith('https'):
+            url = 'http://' + url
+        r = requests.get(url, allow_redirects=True)
+        with open('history.log', 'a') as f:
+            f.write(f"Initial URL: {url} ; Final URL: {r.url} '\n'")
 
-    # Link to url and to index
-    return '''
-    <a href="{}">{}</a><br><br>
-    <a href="/">Back to search</a>
-    '''.format(r.url, r.url)
-
-
-
+        # Link to url and to index
+        return '''
+        <a href="{}">{}</a><br><br>
+        <a href="/">Back to search</a>
+        '''.format(r.url, r.url)
 
 ###########
 
@@ -71,3 +72,5 @@ def process_request():
         </ul>
         <a href="/">Back to search</a>
         '''.format(''.join(['<li><a href="{}">{}</a></li>'.format(url, url) for url, status_code in history]))
+
+app.run(debug=True)
