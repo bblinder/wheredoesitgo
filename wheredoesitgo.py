233d2cd75b7866f5f7e39cc748b60a256a229b33
bblinder <filmmaven@gmail.com>
2022-08-29 10:50:56 -0400

    modified:   wheredoesitgo.py

--- wheredoesitgo.py
+++ wheredoesitgo.py
@@ -18,15 +18,6 @@ logger.info('Started')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return '''
-    <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Redirect</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous>
-        </head>
     <form action="/redirect" method="POST">
         <input type="text" name="url" placeholder="URL">
         <input type="submit" value="Submit">
