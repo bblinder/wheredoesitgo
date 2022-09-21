0034c8b45ebfbc1700108adbd46ed41fc5f8c682
bblinder <filmmaven@gmail.com>
2022-09-21 13:33:49 -0400

    modified:   wheredoesitgo.py

--- wheredoesitgo.py
+++ wheredoesitgo.py
@@ -1,44 +1,47 @@
 # A tool to follow redirects
 
+import logging
+
 import requests
 from flask import Flask, request
-
 
 app = Flask(__name__)
 
 ############
 # This method is if you want to make a request via a browser form.
 
-import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info('Started')
+logger.info("Started")
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return '''
+    return """
     <form action="/redirect" method="POST">
         <input type="text" name="url" placeholder="URL">
         <input type="submit" value="Submit">
     </form>
-    '''
+    """
 
-@app.route('/redirect', methods=['POST'])
+
+@app.route("/redirect", methods=["POST"])
 def redirect_url():
-    url = request.form['url']
-    
+    url = request.form["url"]
+
     # if not URL provided, return error and link back to index
     if not url:
         return '<a href="/">Please enter a URL</a>'
     else:
-        if not url.startswith('http') or not url.startswith('https'):
-            url = 'http://' + url
+        if not url.startswith("http") or not url.startswith("https"):
+            url = "http://" + url
         r = requests.get(url, allow_redirects=True)
-        with open('history.log', 'a') as f:
-            f.write(f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method: GUI '\n'")
+        with open("history.log", "a") as f:
+            f.write(
+                f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method: GUI '\n'"
+            )
 
-        return f'''
+        return f"""
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -57,7 +60,7 @@ def redirect_url():
             </div> 
         </body>
         </html>
-        '''
+        """
 
         # # Link to url and to index
         # return '''
@@ -65,37 +68,45 @@ def redirect_url():
         # <a href="/">Back to search</a>
         # '''.format(r.url, r.url)
 
+
 ###########
 
 # This method is if you want to make a request via curl.
 
-@app.route('/search', methods=['GET'])
+
+@app.route("/search", methods=["GET"])
 def process_request():
-    url = request.args.get('url')
+    url = request.args.get("url")
 
     if not url:
-        return 'No URL provided'
+        return "No URL provided"
     else:
         # add http if it's not there
-        if not url.startswith('http') or not url.startswith('https'):
-            url = 'http://' + url
+        if not url.startswith("http") or not url.startswith("https"):
+            url = "http://" + url
 
         r = requests.get(url, allow_redirects=True)
-        with open('history.log', 'a') as f:
-            f.write(f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method:API '\n'")
-       
+        with open("history.log", "a") as f:
+            f.write(
+                f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method:API '\n'"
+            )
+
         history = [(r.url, r.status_code)]
         while r.history:
             r = r.history[0]
             history.append((r.url, r.status_code))
-    
 
-        return '''
+        return """
         <h1>History</h1>
         <ul>
         {}
         </ul>
         <a href="/">Back to search</a>
-        '''.format(''.join(['<li><a href="{}">{}</a></li>'.format(url, url) for url, status_code in history]))
+        """.format(
+            "".join(
+                [f'<li><a href="{url}">{url}</a></li>' for url, status_code in history]
+            )
+        )
 
-#app.run(port=8080, debug=True)
+
+# app.run(port=8080, debug=True)
