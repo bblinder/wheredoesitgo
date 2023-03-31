7482d5f75e2c18fb11353f70216250ad79433a28
bblinder <filmmaven@gmail.com>
2023-03-30 21:35:36 -0400

    new file:   static/css/styles.css 	new file:   templates/404.html 	new file:   templates/500.html 	new file:   templates/index.html 	new file:   templates/result.html 	modified:   wheredoesitgo.py

--- wheredoesitgo.py
+++ wheredoesitgo.py
@@ -1,110 +1,67 @@
-# A tool to follow redirects
-
 import logging
+from logging.handlers import RotatingFileHandler
 
 import requests
-from flask import Flask, request
+from flask import Flask, abort, render_template, request
 
 app = Flask(__name__)
 
-############
-# This method is if you want to make a request via a browser form.
+log_formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+log_handler = RotatingFileHandler("app.log", maxBytes=10 * 1024 * 1024, backupCount=5)
+log_handler.setFormatter(log_formatter)
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info("Started")
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+logger.info("Application started")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return """
-    <form action="/redirect" method="POST">
-        <input type="text" name="url" placeholder="URL">
-        <input type="submit" value="Submit">
-    </form>
-    """
+    if request.method == "POST":
+        url = request.form["url"]
+
+        if not url:
+            return render_template("index.html", error="Please enter a URL")
+
+        if not (url.startswith("http") or url.startswith("https")):
+            url = "http://" + url
+
+        try:
+            r = requests.get(url, allow_redirects=True, timeout=5)
+            redirect_history = [(resp.url, resp.status_code) for resp in r.history]
+            redirect_history.append((r.url, r.status_code))
+
+            for i, (redirect_url, status_code) in enumerate(redirect_history):
+                logger.info(
+                    "Redirect %d: %s (Status code: %d)",
+                    i + 1,
+                    redirect_url,
+                    status_code,
+                )
+
+            return render_template("result.html", history=redirect_history)
+        except Exception as e:
+            logger.exception("Error processing URL: %s", url)
+            return render_template("index.html", error=f"Error processing URL: {e}")
+
+    return render_template("index.html", error=None)
 
 
-@app.route("/redirect", methods=["POST"])
-def redirect_url():
-    url = request.form["url"]
-
-    # if not URL provided, return error and link back to index
-    if not url:
-        return '<a href="/">Please enter a URL</a>'
-
-    if not url.startswith("http") or not url.startswith("https"):
-        url = "http://" + url
-    r = requests.get(url, allow_redirects=True, timeout=5)
-    with open("history.log", "a") as f:
-        f.write(
-            f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method: GUI '\n'"
-        )
-
-    return f"""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Redirect</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Trace Results</h1>
-            <p>The final URL is: <a href="{r.url}">{r.url}</a></p>
-            <p>Status Code: {r.status_code}</p>
-            <a href="/">Back to home page</a>
-        </div> 
-    </body>
-    </html>
-    """
-
-    # # Link to url and to index
-    # return '''
-    # <a href="{}">{}</a><br><br>
-    # <a href="/">Back to search</a>
-    # '''.format(r.url, r.url)
+@app.errorhandler(404)
+def page_not_found(e):
+    logger.warning("Page not found: %s", request.url)
+    return render_template("404.html"), 404
 
 
-###########
-
-# This method is if you want to make a request via curl.
-
-
-@app.route("/search", methods=["GET"])
-def process_request():
-    url = request.args.get("url")
-
-    if not url:
-        return "No URL provided"
-
-    # add http if it's not there
-    if not url.startswith("http") or not url.startswith("https"):
-        url = "http://" + url
-
-    r = requests.get(url, allow_redirects=True, timeout=5)
-    with open("history.log", "a") as f:
-        f.write(
-            f"Initial URL: {url} ; Final URL: {r.url} ; Status Code:{r.status_code}; Method:API '\n'"
-        )
-
-    history = [(r.url, r.status_code)]
-    while r.history:
-        r = r.history[0]
-        history.append((r.url, r.status_code))
-
-    return """
-    <h1>History</h1>
-    <ul>
-    {}
-    </ul>
-    <a href="/">Back to search</a>
-    """.format(
-        "".join([f'<li><a href="{url}">{url}</a></li>' for url, status_code in history])
-    )
+@app.errorhandler(500)
+def internal_server_error(e):
+    logger.error("Internal server error: %s", request.url)
+    return render_template("500.html"), 500
 
 
-#app.run(port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
